0fcd9e25994b60620d16b6ff56182f3fa6de1d84
gnobaaaar <eumdengs@gmail.com>
2022-01-30 17:46:30 +0900

    Updates

---
+++ precache-manifest.b278b657be497996b42e9be7ae5845c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8de942d589968c05656676d73396e392",
+    "url": "/monsters-app/index.html"
+  },
+  {
+    "revision": "8ec5a4ce32649bd61c98",
+    "url": "/monsters-app/static/css/main.f251e27e.chunk.css"
+  },
+  {
+    "revision": "cdfe034b824a407c32bd",
+    "url": "/monsters-app/static/js/2.1154fe26.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-app/static/js/2.1154fe26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ec5a4ce32649bd61c98",
+    "url": "/monsters-app/static/js/main.deccb1e9.chunk.js"
+  },
+  {
+    "revision": "7138d42281881b672924",
+    "url": "/monsters-app/static/js/runtime-main.ce03e1fd.js"
+  }
+]);
